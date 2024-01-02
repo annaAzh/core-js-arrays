73cd308fc923740770693fdc978b92015cb1f88d
annaAzh <forarre@yandex.ru>
2024-01-02 14:03:20 +0300

    feat: solve getElementByIndices and getFalsyValuesCount tasks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -367,8 +367,8 @@ function generateOdds(/* len */) {
  *   getElementByIndices(['one','two','three'], [2]) => 'three'  (arr[2])
  *   getElementByIndices([[[ 1, 2, 3]]], [ 0, 0, 1 ]) => 2        (arr[0][0][1])
  */
-function getElementByIndices(/* arr, indices */) {
-  throw new Error('Not implemented');
+function getElementByIndices(arr, indices) {
+  return indices.reduce((elem, i) => elem[i], arr);
 }
 
 /**
@@ -383,8 +383,8 @@ function getElementByIndices(/* arr, indices */) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  return arr.filter((item) => !item).length;
 }
 
 /**
